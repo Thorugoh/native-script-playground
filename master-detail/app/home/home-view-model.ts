24e4a8b6e90b74ea0adca555c51d5655a91816e9
Victor Hugo <hugo.victoor77@gmail.com>
2026-05-19 10:53:55 -0300

    create home view model

--- master-detail/app/home/home-view-model.ts
+++ master-detail/app/home/home-view-model.ts
@@ -1,3 +1,20 @@
-import { Observable } from "@nativescript/core";
+import { Observable, ObservableArray } from "@nativescript/core";
+import { FlickModel } from "~/models/flick.model";
+import { FlickService } from "~/services/flick.service";
 
-export class HomeViewModel extends Observable {}
+export class HomeViewModel extends Observable {
+    private _flicks: FlickModel[]
+
+    constructor() {
+        super()
+        this.populateFlicks()
+    }
+
+    get flicks(): ObservableArray<FlickModel> {
+        return new ObservableArray(this._flicks)
+    }
+
+    populateFlicks(): void {
+        this._flicks = FlickService.getInstance().getFlicks()
+    }
+}
