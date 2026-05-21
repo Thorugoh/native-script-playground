6a678edf08baa38977af026edb22db6611038ae9
Victor Hugo <hugo.victoor77@gmail.com>
2026-05-21 09:02:39 -0300

    create ios impl

--- ns-ui-kit/packages/ns-label-marquee/common.ts
+++ ns-ui-kit/packages/ns-label-marquee/common.ts
@@ -1,4 +1,5 @@
 import { Label } from '@nativescript/core';
 
 export class LabelMarqueeCommon extends Label {
+    
 }
--- ns-ui-kit/packages/ns-label-marquee/index.ios.ts
+++ ns-ui-kit/packages/ns-label-marquee/index.ios.ts
@@ -1,5 +1,13 @@
 import { LabelMarqueeCommon } from './common';
 
 export class LabelMarquee extends LabelMarqueeCommon {
+    createNativeView(): Object {
+        return MarqueeLabel.alloc().init();
+    }
 
+    initNativeView(): void {
+        const nativeView = <MarqueeLabel>this.nativeView;
+        nativeView.fadeLength = 10;
+        nativeView.scrollDuration = 8;
+    }
 }
