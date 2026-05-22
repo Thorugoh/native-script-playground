33286771fa9dc218cb819ca1ab34c506d45d9677
Victor Hugo <hugo.victoor77@gmail.com>
2026-05-22 09:17:21 -0300

    register new property fadeLength

--- ns-ui-kit/packages/ns-label-marquee/common.ts
+++ ns-ui-kit/packages/ns-label-marquee/common.ts
@@ -1,5 +1,11 @@
-import { Label } from '@nativescript/core';
+import { Label, Property } from '@nativescript/core';
 
 export class LabelMarqueeCommon extends Label {
-    
+
 }
+
+export const fadeLengthProperty = new Property<LabelMarqueeCommon, number>({
+    name: 'fadeLength'
+})
+
+fadeLengthProperty.register(LabelMarqueeCommon)
--- ns-ui-kit/packages/ns-label-marquee/index.ios.ts
+++ ns-ui-kit/packages/ns-label-marquee/index.ios.ts
@@ -1,4 +1,4 @@
-import { LabelMarqueeCommon } from './common';
+import { fadeLengthProperty, LabelMarqueeCommon } from './common';
 
 export class LabelMarquee extends LabelMarqueeCommon {
     createNativeView(): Object {
@@ -9,5 +9,9 @@ export class LabelMarquee extends LabelMarqueeCommon {
         const nativeView = <MarqueeLabel>this.nativeView;
         nativeView.fadeLength = 10;
         nativeView.scrollDuration = 8;
+    }
+
+    [fadeLengthProperty.setNative](value: number) {
+        (<MarqueeLabel>this.nativeView).fadeLength = value;
     }
 }
