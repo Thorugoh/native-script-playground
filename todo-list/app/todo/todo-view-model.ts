c0746492092d52a15f318ba33c3b27ff0ce006cf
Victor Hugo <hugo.victoor77@gmail.com>
2026-05-24 19:25:24 -0300

    add ui

--- todo-list/app/todo/todo-view-model.ts
+++ todo-list/app/todo/todo-view-model.ts
@@ -1,6 +1,7 @@
 import { Observable } from "@nativescript/core";
 
 export class TodoViewModel extends Observable {
+    public items = [{ title: "test" }]
     constructor() {
         super()
     }
