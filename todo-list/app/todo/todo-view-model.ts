a07dbadef177dcc7d12fcea5d9a58ccd32a16f46
Victor Hugo <hugo.victoor77@gmail.com>
2026-05-24 20:52:29 -0300

    create todo add mehtod

--- todo-list/app/todo/todo-view-model.ts
+++ todo-list/app/todo/todo-view-model.ts
@@ -1,8 +1,22 @@
 import { Observable } from "@nativescript/core";
 
+interface Item {
+    id: string;
+    title: string;
+}
 export class TodoViewModel extends Observable {
-    public items = [{ title: "test" }]
+    private _items = [{ title: "test", id: crypto.randomUUID() }]
     constructor() {
         super()
     }
+
+    get items() {
+        return this._items;
+    }
+
+    addTodo({ title }: Item) {
+        this._items.push({ title, id: crypto.randomUUID() })
+    }
+
+
 }
