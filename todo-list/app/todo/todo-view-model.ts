71452101f422bc860abe1c2fd46a4f99fd4ba185
Victor Hugo <hugo.victoor77@gmail.com>
2026-05-25 08:26:25 -0300

    add item

--- todo-list/app/todo/todo-view-model.ts
+++ todo-list/app/todo/todo-view-model.ts
@@ -5,18 +5,26 @@ interface Item {
     title: string;
 }
 export class TodoViewModel extends Observable {
+    private _newItem = "";
     private _items = [{ title: "test", id: crypto.randomUUID() }]
     constructor() {
         super()
+    }
+
+    get newItem(){
+        return this._newItem
+    }
+
+    set newItem(text: string){
+        this._newItem = text;
     }
 
     get items() {
         return this._items;
     }
 
-    addTodo({ title }: Item) {
-        this._items.push({ title, id: crypto.randomUUID() })
+    addTodo() {
+        console.log({ item: this.newItem })
+        this._items.push({ title: this.newItem, id: crypto.randomUUID() })
     }
-
-
 }
