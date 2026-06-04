b0d607a8e86fd3b2af73adbf844c225db4d97e6b
Victor Hugo <hugo.victoor77@gmail.com>
2026-06-04 09:37:05 -0300

    remove old unused todo files

--- todo-list/app/todo/todo-page.ts
+++
@@ -1,7 +0,0 @@
-import { EventData, Page } from "@nativescript/core";
-import { TodoViewModel } from "./todo-view-model";
-
-export function navigatingTo(args: EventData) {
-    const page = <Page>args.object;
-    page.bindingContext = new TodoViewModel();
-}
