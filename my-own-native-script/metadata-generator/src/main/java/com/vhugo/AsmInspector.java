a24fb707745d08f07c254f2580c941223aa8180f
Victor Hugo <hugo.victoor77@gmail.com>
2026-06-11 22:55:15 -0300

    generate header file

--- my-own-native-script/metadata-generator/src/main/java/com/vhugo/AsmInspector.java
+++ my-own-native-script/metadata-generator/src/main/java/com/vhugo/AsmInspector.java
@@ -5,9 +5,8 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -15,30 +14,55 @@ public class AsmInspector {
     public static void main(String[] args) {
         Path classFilePath = Paths.get("../math-app/target/classes/com/vhugo/MathUtils.class");
 
-        System.out.println("Looking for class at: " + classFilePath.toAbsolutePath());
+        Path outputHeaderDir = Paths.get("../cpp-bridge");
+        Path outputHeaderPath = outputHeaderDir.resolve("Metadata.h");
 
-        try (FileInputStream fileInputStream = new FileInputStream(classFilePath.toFile())) {
-            ClassReader classReader = new ClassReader(fileInputStream);
-            ClassVisitor classVisitor = new ClassVisitor(Opcodes.ASM9) {
-                @Override
-                public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-                    System.out.println("Inspecting Class: "+ name);
-                }
+        try {
+            if (!Files.exists((outputHeaderDir))) {
+                Files.createDirectories(outputHeaderDir);
+            }
 
-                @Override
-                public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
-                    if(!name.equals("<init>")) {
-                        System.out.println("Method name: " + name);
-                        System.out.println("JNI Descriptor: " + descriptor);
-                        System.out.println("---");
+            try (FileInputStream fileInputStream = new FileInputStream(classFilePath.toFile());
+                 PrintWriter writer = new PrintWriter(new FileWriter(outputHeaderPath.toFile()))) {
+
+                writer.println("#ifndef METADATA_H");
+                writer.println("#define METADATA_H");
+                writer.println();
+                writer.println("#include <string>");
+                writer.println();
+
+                ClassReader classReader = new ClassReader(fileInputStream);
+
+                ClassVisitor classVisitor = new ClassVisitor(Opcodes.ASM9) {
+                    private String currentClassName;
+
+                    @Override
+                    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+                        System.out.println("Inspecting Class: " + name);
+                        currentClassName = name;
                     }
-                    return super.visitMethod(access, name, descriptor, signature, exceptions);
-                }
 
-            };
+                    @Override
+                    public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+                        if (!name.equals("<init>")) {
+                            String upperName = name.toUpperCase();
 
-            classReader.accept(classVisitor, 0);
+                            writer.println("namespace JniMetadata{");
+                            writer.println("   const std::string CLASS_" + upperName + " = \"" + currentClassName + "\";");
+                            writer.println("   const std::string METHOD_" + upperName + " = \"" + name + "\";");
+                            writer.println("   const std::string SIG_" + upperName + " = \"" + descriptor + "\";");
+                            writer.println("}");
+                            writer.println();
+                        }
+                        return super.visitMethod(access, name, descriptor, signature, exceptions);
+                    }
 
+                };
+
+                classReader.accept(classVisitor, 0);
+                writer.println("#endif // METADATA_H");
+                System.out.println("Success! Generated C++ Header at: " + outputHeaderPath.toAbsolutePath());
+            }
         } catch (IOException e) {
             System.err.println("Error: Class file not found. Did you build the 'math-app' module first?");
             e.printStackTrace();
