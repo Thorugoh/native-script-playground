6ebefcaffe0b122ed460425332d9ee3f603871c8
Victor Hugo <hugo.victoor77@gmail.com>
2026-06-24 23:06:20 -0300

    add native functions

--- battery-checker/app/main-view-model.ts
+++ battery-checker/app/main-view-model.ts
@@ -9,6 +9,26 @@ function batteryLevel(): number {
   return bm.getIntProperty(android.os.BatteryManager.BATTERY_PROPERTY_CAPACITY);
 }
 
+function vibrate(ms: number): void {
+  const v = ctx().getSystemService(android.content.Context.VIBRATOR_SERVICE) as android.os.Vibrator;
+  if (android.os.Build.VERSION.SDK_INT >= 26) {
+    v.vibrate(
+      android.os.VibrationEffect.createOneShot(
+        ms,
+        android.os.VibrationEffect.DEFAULT_AMPLITUDE
+      )
+    );
+  } else {
+    v.vibrate(ms); // fallback API < 26
+  }
+}
+
+function toast(msg: string): void {
+  android.widget.Toast
+    .makeText(ctx(), msg, android.widget.Toast.LENGTH_SHORT)
+    .show();
+}
+
 export function createViewModel(): Observable {
   const vm = new Observable();
 
@@ -19,6 +39,16 @@ export function createViewModel(): Observable {
     const level = batteryLevel();
     vm.set("battery", level);
     vm.set("log", `Battery read via BatteryManager: ${level}`)
+  });
+
+  vm.set("onVibrate", () => {
+    vibrate(400)
+    vm.set('log', 'vibrate via android.os.VibrationEffect.');
+  });
+
+  vm.set('onToast', () => {
+    toast('Hi L&L 👋');
+    vm.set('log', 'Toast triggered via android.widget.Toast.');
   });
 
   return vm;
